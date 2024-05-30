30eb264dd397bdf73d68defe4bfcf53644168b6d
小恸恸 <wtt150249@gmail.com>
2024-05-30 22:12:56 +0800

    feat: 添加 icon

---
+++ components/icon/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<i
+		ref="icon"
+		class="icon"
+		:style="{
+			blockSize: typeof props.size === 'number' ? `${props.size}px` : props.size
+		}"
+	>
+		<template v-if="props.icon === 'cat'">
+			<myCatIcon class="svg" />
+		</template>
+	</i>
+</template>
+
+<script setup lang="ts">
+import myCatIcon from "./icons/cat.vue";
+
+interface Props {
+	icon: "cat";
+	size?: number | string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	icon: "cat",
+	size: 24
+});
+</script>
+
+<style scoped>
+.icon {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	aspect-ratio: 1;
+}
+
+.svg {
+	width: 100%;
+	height: 100%;
+}
+</style>
--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import roughLine from "./components/line/rough/index.vue";
 import roughCard from "./components/card/rough/index.vue";
 import roughIcon from "./components/icon/rough/index.vue";
 import webBG from "./components/web-bg/index.vue";
+import icon from "./components/icon/index.vue";
 
 const withInstall = (app: App) => {
 	app.component("namiRoughLine", roughLine);
 	app.component("namiRoughCard", roughCard);
 	app.component("namiRoughIcon", roughIcon);
+	app.component("namiIcon", icon);
 	app.component("namiWebBG", webBG);
 };
 
-export { roughLine, roughCard, roughIcon, webBG, withInstall, withInstall as default };
+export { roughLine, roughCard, roughIcon, icon, webBG, withInstall, withInstall as default };
