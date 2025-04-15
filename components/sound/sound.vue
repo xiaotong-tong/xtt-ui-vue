025dea8a20b85eef05fec0d7ce8dc2ae2c60e325
小恸恸 <wtt150249@gmail.com>
2025-04-15 22:02:38 +0800

    feat: 更新音频组件以支持动态预加载属性

--- components/sound/sound.vue
+++ components/sound/sound.vue
@@ -1,6 +1,6 @@
 <template>
 	<Icon class="soundTrigger" icon="mdiVolumeHigh" v-bind="$attrs" @click="play" v-show="audioCanplay"></Icon>
-	<audio ref="audioRef" preload="metadata" :src="src" @canplay="canplay"></audio>
+	<audio ref="audioRef" :preload="preload" :src="src" @canplay="canplay"></audio>
 </template>
 
 <script setup lang="ts">
@@ -9,22 +9,38 @@ import Icon from "../icon/icon.vue";
 
 interface Props {
 	src: string;
+	preload?: "none" | "metadata" | "auto";
 }
 
-const { src } = defineProps<Props>();
+const { src, preload = "metadata" } = defineProps<Props>();
 
 const audioRef = useTemplateRef<HTMLAudioElement>("audioRef");
 
+const playing = ref(false);
 const play = () => {
-	audioRef.value?.play().catch((error) => {
-		console.error("Error playing audio:", error);
-	});
+	if (playing.value) {
+		audioRef.value?.pause();
+		playing.value = false;
+		return;
+	}
+
+	audioRef.value
+		?.play()
+		.then(() => {
+			playing.value = true;
+		})
+		.catch((error) => {
+			console.error("Error playing audio:", error);
+		});
 };
 
 const audioCanplay = ref(false);
 const canplay = () => {
 	audioCanplay.value = true;
 };
+if (preload === "none") {
+	audioCanplay.value = true;
+}
 </script>
 
 <style scoped>
